944407a236f04517189c4abf7464c52f388cc9ab
RK03ind <gamitech95@gmail.com>
2024-08-23 12:51:43 +0530

    ADD: Better Search/Filter Logic

--- src/pages/PokemonList/PokemonList.jsx
+++ src/pages/PokemonList/PokemonList.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import axios from "axios";
 import PokemonCard from "../../components/PokemonCard";
@@ -5,23 +6,12 @@ import "./PokemonList.css";
 
 const PokemonList = () => {
   const [pokemons, setPokemons] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
+  const [currentPokemons, setCurrentPokemons] = useState([]);
   const [search, setSearch] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(Math.ceil(1302 / 20));
   const [loading, setLoading] = useState(false);
-  const itemsPerPage = 16;
-
-  useEffect(() => {
-    const fetchTotalCount = async () => {
-      const response = await axios.get(
-        "https://pokeapi.co/api/v2/pokemon?limit=1"
-      );
-      const count = response.data.count;
-      setTotalPages(Math.ceil(count / itemsPerPage));
-    };
-
-    fetchTotalCount();
-  }, []);
+  const itemsPerPage = 20;
 
   useEffect(() => {
     const fetchPokemons = async () => {
@@ -39,11 +29,39 @@ const PokemonList = () => {
       setTimeout(() => {
         setPokemons(data);
         setLoading(false);
-      }, 650); // 650ms delay to test loading animation
+      }, 500); // 500ms delay to test loading animation
     };
 
     fetchPokemons();
   }, [currentPage]);
+
+  useEffect(() => {
+    if (search) {
+      const fetchPokemon = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(
+            `https://pokeapi.co/api/v2/pokemon/${search.trim().toLowerCase()}`
+          );
+          setCurrentPokemons([response.data]);
+        } catch (err) {
+          const filteredPokemons = pokemons.filter((pokemon) =>
+            pokemon.name.toLowerCase().includes(search.toLowerCase())
+          );
+          setCurrentPokemons(filteredPokemons); // No direct pokemon found list filtered
+        }
+        setLoading(false);
+      };
+
+      fetchPokemon();
+    } else {
+      setCurrentPokemons(pokemons);
+    }
+  }, [search, pokemons]);
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
   const handleNextPage = () => {
     if (currentPage < totalPages) {
@@ -57,38 +75,43 @@ const PokemonList = () => {
     }
   };
 
-  const filteredPokemons = pokemons.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(search.toLowerCase())
-  );
-
   return (
     <div>
       <input
         type="text"
-        placeholder="Search Pokémon..."
-        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search Pokémon by name or ID..."
+        onChange={handleSearch}
         value={search}
       />
       {loading ? (
         <div className="loading">Loading...</div>
       ) : (
         <div className="pokemon-list">
-          {filteredPokemons.map((pokemon) => (
-            <PokemonCard key={pokemon.id} pokemon={pokemon} />
-          ))}
+          {currentPokemons.length != 0 ? (
+            currentPokemons.map((pokemon) => (
+              <PokemonCard key={pokemon.id} pokemon={pokemon} />
+            ))
+          ) : (
+            <h2>No valid matches found :(</h2>
+          )}
         </div>
       )}
-      <div className="pagination">
-        <button onClick={handlePrevPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span>
-          Page {currentPage} of {totalPages}
-        </span>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
+      {!search && (
+        <div className="pagination">
+          <button onClick={handlePrevPage} disabled={currentPage === 1}>
+            Previous
+          </button>
+          <span>
+            Page {currentPage} of {totalPages}
+          </span>
+          <button
+            onClick={handleNextPage}
+            disabled={currentPage === totalPages}
+          >
+            Next
+          </button>
+        </div>
+      )}
     </div>
   );
 };
